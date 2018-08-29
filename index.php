90a33aa5a80e3e8bfc18b7fd603f14418743bc9c
Adrien <adrien.dau@gmail.com>
2018-08-29 16:37:55 +0200

    Remplacer preg_replace par Transliterator
    https://www.matthecat.com/supprimer-les-accents-dune-chaine-en-php/

--- index.php
+++ index.php
@@ -5,13 +5,17 @@ header("Content-Type: application/rss+xml; charset=UTF-8");
 $url = 'https://1001ebooks.com';
 $xml = simplexml_load_file($url . '/feed/', 'SimpleXMLElement', LIBXML_NOCDATA);
 
+$transliterator = Transliterator::createFromRules(
+    "[^[:L:][:N:].]+ > '-';"    // Remplace tout ce qui n'est pas une lettre, un chiffre ou un point par un tiret
+);
+
 foreach ($xml->channel->item as $item) {
     $picture = sprintf(
         '%s/wp-content/uploads/%s/%s/%s.jpg',
         $url,
         date('Y', strtotime($item->pubDate)),
         date('m', strtotime($item->pubDate)),
-        preg_replace(array('/(– |’)|[^A-zÀ-ÿ0-9 \.-]/', '/ /'), array('', '-'), $item->title)
+        trim($transliterator->transliterate($item->title), '-')
     );
 
     $item->description = '<p><img src="' . $picture . '" alt="couverture"></p>';
